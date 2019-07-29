513141af6160636ae238c843535c0bfd7143cdb1
Jonathan LM Diniz <jonathan.lucas.m@gmail.com>
2019-07-29 17:30:48 -0300

    Adicionado fundo para o menu

--- js/menu.js
+++ js/menu.js
@@ -8,13 +8,16 @@ class menu extends Phaser.Scene {
         this.load.image('menuprocessos','img/menuprocessos.png');
         this.load.image('menudispositivos','img/menudispositivos.png');
         this.load.image('menuarquivos','img/menuarquivos.png');
+        this.load.image('menufundo','img/menufundo2.png');
     }
     create() {
+        var menufundo = this.add.image(0,0,'menufundo').setOrigin(0,0);
+        menufundo.setScale(1.5,1.7);
         var imagemprocessos = this.add.image(200,150, 'menuprocessos').setInteractive();
         var imagemmemoria = this.add.image(600,150, 'menumemoria').setInteractive();
         var imagemarquivos = this.add.image(200,450, 'menuarquivos').setInteractive();
         var imagemdispositivos = this.add.image(600,450, 'menudispositivos').setInteractive();
-
+        
         imagemmemoria.on('pointerover', function () {
             this.setTint(0x00ff00);
         });
